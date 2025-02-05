fb93f1fa2ab410928e293a453cd89737d0d6654a
sakawa bob <bob.sakawa@gmail.com>
2025-02-05 23:03:14 +0300

    partial order impl

---
+++ order-service-ms/src/main/java/com/techsurvy/orderservicems/dto/OrderRequestDto.java
@@ -0,0 +1,17 @@
+package com.techsurvy.orderservicems.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+public class OrderRequestDto {
+    private Integer userId;
+    private Integer productId;
+    private UUID orderId;
+
+}
